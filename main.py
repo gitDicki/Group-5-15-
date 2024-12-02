2755bab0a3e3f2519d0618af06f71891134abece
gitDicki <baytalohastop@gmail.com>
2024-12-03 00:08:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#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
-
 import pygame
 import sys
 import random
@@ -28,7 +26,7 @@ def endgame(game, ideal):
                 fail += 1
     return fail
 
-def draw_popup_message(message, color=WHITE):#Это для окна
+def draw_popup_message(message, color=WHITE):  # Это для окна
     # Создание поверхности для текста
     text_surface = font.render(message, True, color)
 
@@ -42,24 +40,24 @@ def draw_popup_message(message, color=WHITE):#Это для окна
     # Отрисовка текста
     screen.blit(text_surface, text_rect)
 
-#функция для отрисовки клеток
+# функция для отрисовки клеток
 def draw_grid(x2, y2):
     for row in range(GRID_SIZE):
         for col in range(GRID_SIZE):
             num = numbers[row][col]
             x = col * CELL_SIZE
             y = row * CELL_SIZE
-            
-            #определяем цвет квадрата
+
+            # определяем цвет квадрата
             if row == y2 and col == x2:
-                color = CHET  #цвет выделенного квадрата
+                color = CHET  # цвет выделенного квадрата
             else:
                 color = GRAY if num is not None else WHITE
-            
-            #отрисовка квадратов
+
+            # отрисовка квадратов
             pygame.draw.rect(screen, color, (x, y, CELL_SIZE, CELL_SIZE))
-            
-            #отрисовка числа
+
+            # отрисовка числа
             if num is not None:
                 if num != 0:
                     font = pygame.font.Font(None, 74)
@@ -78,19 +76,19 @@ def start():
     sum = []
     for i in range(16):  # Генерируем 16 рандомных чисел и записываем их в sum=[]
         sum.append(count)
-        count += 1  
+        count += 1
     random.shuffle(sum)  # Перемешиваем эти 16 чисел
     count = 0
     for i in range(4):  # Присваиваем эти числа к ячейкам массива
         for j in range(4):
             game[i][j] = sum[count]
-            count += 1      
+            count += 1
     zero = [0, 0]
     for i in range(4):  # Ищем нолик в матрице
         for j in range(4):
             if game[i][j] == 0:
                 zero[0] = i
-                zero[1] = j             
+                zero[1] = j
     return game
 
 numbers = start()
@@ -110,6 +108,7 @@ for i in range(4):
         ideal[i][j] = count
         count += 1
 ideal[3][3] = 0
+g = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -117,13 +116,12 @@ while True:
             pygame.quit()
             sys.exit()
 
-        
-        #перемещение по полю
+        # перемещение по полю
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 if y != 0:
                     y -= 1
-                    print(numbers[y][x])    
+                    print(numbers[y][x])
             if event.key == pygame.K_DOWN:
                 if y != 3:
                     y += 1
@@ -137,73 +135,17 @@ while True:
                     x += 1
                     print(numbers[y][x])
 
-
-
-            if event.key == pygame.K_UP and\
+            if event.key == pygame.K_UP and \
                     (event.mod & pygame.KMOD_SHIFT):
-                    if numbers[y][x] == 0:
-                        numbers[y][x], numbers[y+1][x] = numbers[y+1][x], numbers[y][x]
-                        if endgame(numbers, ideal) == 0:
-                            draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
-                            pygame.display.flip()  # Обновление экрана
-                            pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                            screen.fill(BLACK)  # Очистка экрана после задержки
-                            pygame.display.flip()
-                            sys.exit()
-                    else:
-                        draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
+                if g == 0:
+                    numbers[y][x], numbers[y + 1][x] = numbers[y + 1][x], numbers[y][x]
+                    if endgame(numbers, ideal) == 0:
+                        draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
                         pygame.display.flip()  # Обновление экрана
                         pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
                         screen.fill(BLACK)  # Очистка экрана после задержки
                         pygame.display.flip()
-
-            if event.key == pygame.K_DOWN and\
-                    (event.mod & pygame.KMOD_SHIFT):
-                    if numbers[y][x] == 0:
-                        numbers[y][x], numbers[y-1][x] = numbers[y-1][x], numbers[y][x]
-                        if endgame(numbers, ideal) == 0:
-                            draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
-                            pygame.display.flip()  # Обновление экрана
-                            pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                            screen.fill(BLACK)  # Очистка экрана после задержки
-                            pygame.display.flip()
-                            sys.exit()
-                    else:
-                        draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
-                        pygame.display.flip()  # Обновление экрана
-                        pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                        screen.fill(BLACK)  # Очистка экрана после задержки
-                        pygame.display.flip()
-
-            if event.key == pygame.K_LEFT and\
-                    (event.mod & pygame.KMOD_SHIFT):
-                    if numbers[y][x] == 0:
-                        numbers[y][x], numbers[y][x+1] = numbers[y][x+1], numbers[y][x]
-                        if endgame(numbers, ideal) == 0:
-                            draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
-                            pygame.display.flip()  # Обновление экрана
-                            pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                            screen.fill(BLACK)  # Очистка экрана после задержки
-                            pygame.display.flip()
-                            sys.exit()
-                    else:
-                        draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
-                        pygame.display.flip()  # Обновление экрана
-                        pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                        screen.fill(BLACK)  # Очистка экрана после задержки
-                        pygame.display.flip()
-
-            if event.key == pygame.K_RIGHT and\
-                    (event.mod & pygame.KMOD_SHIFT):
-                if numbers[y][x] == 0:
-                        numbers[y][x], numbers[y][x-1] = numbers[y][x-1], numbers[y][x]
-                        if endgame(numbers, ideal) == 0:
-                            draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
-                            pygame.display.flip()  # Обновление экрана
-                            pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
-                            screen.fill(BLACK)  # Очистка экрана после задержки
-                            pygame.display.flip()
-                            sys.exit()
+                        sys.exit()
                 else:
                     draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
                     pygame.display.flip()  # Обновление экрана
@@ -211,7 +153,59 @@ while True:
                     screen.fill(BLACK)  # Очистка экрана после задержки
                     pygame.display.flip()
 
+            if event.key == pygame.K_DOWN and \
+                    (event.mod & pygame.KMOD_SHIFT):
+                if g == 0:
+                    numbers[y][x], numbers[y - 1][x] = numbers[y - 1][x], numbers[y][x]
+                    if endgame(numbers, ideal) == 0:
+                        draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
+                        pygame.display.flip()  # Обновление экрана
+                        pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                        screen.fill(BLACK)  # Очистка экрана после задержки
+                        pygame.display.flip()
+                        sys.exit()
+                else:
+                    draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
+                    pygame.display.flip()  # Обновление экрана
+                    pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                    screen.fill(BLACK)  # Очистка экрана после задержки
+                    pygame.display.flip()
 
+            if event.key == pygame.K_LEFT and \
+                    (event.mod & pygame.KMOD_SHIFT):
+                if g == 0:
+                    numbers[y][x], numbers[y][x + 1] = numbers[y][x + 1], numbers[y][x]
+                    if endgame(numbers, ideal) == 0:
+                        draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
+                        pygame.display.flip()  # Обновление экрана
+                        pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                        screen.fill(BLACK)  # Очистка экрана после задержки
+                        pygame.display.flip()
+                        sys.exit()
+                else:
+                    draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
+                    pygame.display.flip()  # Обновление экрана
+                    pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                    screen.fill(BLACK)  # Очистка экрана после задержки
+                    pygame.display.flip()
+
+            if event.key == pygame.K_RIGHT and \
+                    (event.mod & pygame.KMOD_SHIFT):
+                if g == 0:
+                    numbers[y][x], numbers[y][x - 1] = numbers[y][x - 1], numbers[y][x]
+                    if endgame(numbers, ideal) == 0:
+                        draw_popup_message("ПОБЕДА!! ГОЙДА!! ГОЛ!!", RED)
+                        pygame.display.flip()  # Обновление экрана
+                        pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                        screen.fill(BLACK)  # Очистка экрана после задержки
+                        pygame.display.flip()
+                        sys.exit()
+                else:
+                    draw_popup_message("Так эту ячейку не сдвинешь!", GREEN)
+                    pygame.display.flip()  # Обновление экрана
+                    pygame.time.delay(1500)  # Задержка перед исчезновением сообщения
+                    screen.fill(BLACK)  # Очистка экрана после задержки
+                    pygame.display.flip()
 
     screen.fill(WHITE)
     draw_grid(x, y)
